87f5b7b96af48bf8175c63e8e37637ae8b63ff1b
Chuka Ebi <chuka.ebi@outlook.com>
2015-10-13 11:36:06 +0100

    Updated Unit Test and passed it

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 require 'DockingStation'
 describe DockingStation do
-  it 'should respond to release bike' do
-    respond_to(release_bike)
+  it 'responds to release bike' do
+    expect(subject).to respond_to :release_bike
   end
 end
