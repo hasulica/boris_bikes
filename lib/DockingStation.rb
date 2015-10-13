f0798aa2b87a4a23bdae7e0dcb8e3bccb3e0b110
Chuka Ebi <chuka.ebi@outlook.com>
2015-10-13 16:05:04 +0100

    Updated unit tests, and added ability to hold up to 20 bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,22 +1,22 @@
 require_relative 'Bike'
 class DockingStation
-  attr_reader :arr
+  attr_reader :bikes
 
   def initialize
-    @arr = []
+    @bikes = []
   end
 
-  def release_bike
-    raise StandardError, "No Bikes Available" unless @arr.length > 0
-    @arr.pop
+  def release_bikes
+    raise StandardError, "No Bike Available" unless @bikes.length > 0
+    @bikes.pop
   end
 
-  def docks_bike(b)
-    raise StandardError, "Station at Capacity" unless @arr.length == 0
-    @arr.push(b)
+  def docks_bikes(b)
+    raise StandardError, "Station at Capacity" unless @bikes.length < 20
+    @bikes.push(b)
   end
 
   def show
-    @arr.last
+    @bikes.last
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,39 +1,38 @@
 require 'DockingStation'
 describe DockingStation do
-  it 'responds to release bike' do
-    expect(subject).to respond_to :release_bike
+  it 'responds to release bikes' do
+    expect(subject).to respond_to :release_bikes
   end
 
-  it 'releases working bike' do
+  it 'releases working bikes' do
     bike = Bike.new
-    subject.docks_bike(bike)
-    expect(subject.release_bike).to be_working
+    subject.docks_bikes(bike)
+    expect(subject.release_bikes).to be_working
   end
 
-  it 'responds to docks_bike' do
-    expect(subject).to respond_to(:docks_bike).with(1).argument
+  it 'responds to docks_bikes' do
+    expect(subject).to respond_to(:docks_bikes).with(1).argument
   end
 
-  it 'docks bike' do
+  it 'docks bikes' do
     bi = Bike.new
-    subject.docks_bike(bi)
-    expect(subject.arr).to include(bi)
+    subject.docks_bikes(bi)
+    expect(subject.bikes).to include(bi)
   end
 
-  it 'shows docked bike' do
+  it 'shows docked bikes' do
     bi = Bike.new
-    subject.docks_bike(bi)
+    subject.docks_bikes(bi)
     expect(subject.show).to eq bi
   end
 
-  it 'expects a No Bikes Available error' do
-    expect {subject.release_bike}.to raise_error("No Bikes Available")
+  it 'expects a No Bike Available error' do
+    expect {subject.release_bikes}.to raise_error("No Bike Available")
   end
 
   it 'expects a Station at Capacity error' do
-    bike = Bike.new
-    subject.docks_bike(bike)
-    expect {subject.docks_bike(bike)}.to raise_error("Station at Capacity")
+    20.times{subject.docks_bikes(Bike.new)}
+    expect {subject.docks_bikes(Bike.new)}.to raise_error("Station at Capacity")
 
   end
 end
