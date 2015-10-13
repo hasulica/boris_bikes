6d1462730af61161c3fbcdb2547517fc6e7d4fdb
hasulica <raduasandei@gmail.com>
2015-10-13 16:14:22 +0100

    Refactored using SRP.

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -7,16 +7,26 @@ class DockingStation
   end
 
   def release_bikes
-    raise StandardError, "No Bike Available" unless @bikes.length > 0
+    raise StandardError, "No Bike Available" unless !empty?
     @bikes.pop
   end
 
   def docks_bikes(b)
-    raise StandardError, "Station at Capacity" unless @bikes.length < 20
+    raise StandardError, "Station at Capacity" unless !full?
     @bikes.push(b)
   end
 
   def show
     @bikes.last
   end
+
+  private
+  def full?
+    @bikes.length == 20
+  end
+
+  def empty?
+    @bikes.any? ? false : true
+  end
+
 end
